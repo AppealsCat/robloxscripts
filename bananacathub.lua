47ed2a58c079f13217882be7a2e28cc3812ad093
AppealsCat <84909028+AppealsCat@users.noreply.github.com>
2025-02-15 17:15:16 +0700

    Update bananacathub.lua

--- bananacathub.lua
+++ bananacathub.lua
@@ -1,5 +1,5 @@
 Get a Free Key from this link: https://ads.luarmor.net/get_key?for=VHFslhWdrPey
 
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
-getgenv().Key = "Input Key" 
+getgenv().Key = "Put the Key here" 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
